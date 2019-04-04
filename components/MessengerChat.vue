b4d882fb90483b4c41b9ee7f2f1ca847f5c0d2ae
Aleksander Grygier <alek@grygier.co>
2019-04-04 02:06:09 +0200

    Updated README.md + added blank MessengerChat component

---
+++ components/MessengerChat.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    id="vsf-messenger-chat"
+  >
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MessengerChat',
+  props: {
+    theme_color: {
+      type: String,
+      required: false
+    },
+    logged_in_greeting: {
+      type: String,
+      required: false
+    },
+    logged_out_greeting : {
+      type: String,
+      required: false
+    },
+    greeting_dialog_display: {
+      type: String,
+      required: false,
+      default: 'hide'
+    },
+    greeting_dialog_delay: {
+      type: Boolean,
+      required: false
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+#vsf-messenger-chat {
+  position: fixed;
+  z-index: 999999999999999999999999999999;
+}
+
+</style>
