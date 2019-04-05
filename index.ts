ebd1d2aa5d05bf82e8851698b70e62cdeac76d17
Fifciuu <filip.jdrasik@gmail.com>
2019-04-05 15:21:48 +0200

    Minimal working version

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import { afterRegistration } from './hooks/afterRegistration'
+import { beforeRegistration } from './hooks/beforeRegistration.ts'
 import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/core/lib/module'
 
 export const KEY = 'vsf-messenger-chat'
 
 const moduleConfig: VueStorefrontModuleConfig = {
   key: KEY,
-  afterRegistration
+  beforeRegistration
 }
 
 export const VsfMessengerChat = new VueStorefrontModule(moduleConfig)
