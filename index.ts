e174d81007799fc5977dbc03aeb0d76d12dc6355
Fifciuu <filip.jdrasik@gmail.com>
2019-05-03 16:28:18 +0200

    Removed vsf- prefix

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { beforeRegistration } from './hooks/beforeRegistration.ts'
 import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/core/lib/module'
 
-export const KEY = 'vsf-messenger-chat'
+export const KEY = 'messenger-chat'
 
 const moduleConfig: VueStorefrontModuleConfig = {
   key: KEY,
